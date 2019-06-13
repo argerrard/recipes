f168886b4f07ccb42ff4aa6b83e6fcaa07d3f12f
Andrew Gerrard <argerrard@live.com>
2019-06-13 14:44:31 -0400

    working connection to PSQL database

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
+const psqlConfig = require('./config/psql.config');
+
+
+const { Pool } = require('pg')
+
+const pool = new Pool(psqlConfig);
 
 const port = process.env.PORT || 5000;
+
+pool.query('SELECT * FROM test2 WHERE name = $1', [1], (error, results) => {
+    if (error) {
+      throw error
+    }
+    console.log(results);
+  });
 
 
 app.use(express.json());
