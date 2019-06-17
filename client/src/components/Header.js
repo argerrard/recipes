f04ddc2dfda4e92431b8e9858653fa8a3fc9cefa
Andrew Gerrard <argerrard@live.com>
2019-06-16 22:28:09 -0400

    Created sample logged in Header vs logged out Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Dropdown, Button, Menu, Icon } from 'semantic-ui-react';
 
 class Header extends React.Component {
 
+    renderDefault() {
+        return (
+            <>
+            <Menu.Item>
+                <Button primary as={Link} to="/login">Login</Button>
+            </Menu.Item>
+            <Menu.Item>
+                <Button primary as={Link} to="/signup">Sign Up</Button>
+            </Menu.Item>
+            </>
+        );
+    }
+
+    renderLoggedIn() {
+        return(
+            <>
+            <Menu.Item>
+                <Button primary as={Link} to="/recipes">Recipes</Button>
+            </Menu.Item>
+            <Menu.Item>
+                <Button primary as={Link} to="/ingredients">Ingredients</Button>
+            </Menu.Item>
+            <Menu.Item>
+                <Dropdown icon={<Icon name="user circle" color="blue" size="large" />}>
+                    <Dropdown.Menu>
+                    <Dropdown.Item text='My Profile' as={Link} to="/profile" />
+                    <Dropdown.Item text='My Friends' as={Link} to="/friends" />
+                    <Dropdown.Divider />
+                    <Dropdown.Item text='Logout' as={Link} to="/" />
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Menu.Item>
+            </>
+        );
+    }
+
     render () {
         return (
-            <div className="ui large menu">
-                <Link to="/" className="header item">
+            <Menu>
+                <Menu.Item header as={Link} to="/">
                     Recipes
-                </Link>
-                <div className="right menu">
-                    <Link to="/login" className="ui primary button">Login</Link>
-                    <Link to="/signup" className="ui primary button">Sign Up</Link>
-                </div>
-            </div>
+                </Menu.Item>
+                <Menu.Menu position='right'>
+                    {this.renderLoggedIn()}
+                </Menu.Menu>
+            </Menu>
         );
     }
 
