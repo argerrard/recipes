472baa6e3ac3f22ef67b5a85ceae078bb4e268ad
Andrew Gerrard <argerrard@live.com>
2019-07-08 18:49:37 -0400

    Added server dependency for bcrypt, created registration api route

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,13 +4,40 @@ const db = require('../../db');
 
 // @route    GET api/users
 // @desc     Get all users
-// @access   Private
+// @access   Private (admin only)
 router.get('/', (req, res) => {
     db.query('SELECT * FROM AppUser;',[], (err, result) => {
         console.log(result);
         res.send({
             rows: result.rows
         });
+    });
+});
+
+// @route    POST api/users
+// @desc     Create a new user
+// @access   Public
+router.post('/', (req, res) => {
+
+    const { username, email, password, passwordConfirm } = req.body;
+    const errors = [];
+
+    //Validation
+    //Ensure required fields exist
+    if (!username) errors.push('Registration requires a username.');
+    if (!email) errors.push('Registration requires a valid e-mail');
+    if (!password) errors.push('Registration requires a password');
+    if (!passwordConfirm) errors.push('Please confirm your password to register.');
+
+    if (errors) {
+        res.status(400).json({
+            errors
+        });
+        return;
+    }
+
+    res.json({
+        message: 'Registered new user'
     });
 });
 
