b4a6ca7d07ec63114de712e9303ceac8e8c3b2a4
Andrew Gerrard <argerrard@live.com>
2019-07-09 09:56:50 -0400

    added validation for user login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,3 +5,46 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('../../config/config.json'); 
 const jwtSecret = config.jwtSecret;
+
+// @route    POST api/auth
+// @desc     Authenticate a user and provide them with a token
+// @access   Public
+router.post('/', async (req, res) => {
+
+    const { username, password } = req.body;
+    const errors = [];
+
+    //Validate provided username and password
+    if (!username || !password) errors.push('Please provide a username and password.');
+
+    //Confirm that username exists before attempting login
+    try {
+        const result = await db.query('SELECT username FROM appuser WHERE username=$1;', [username]);
+        if (result.rowCount == 0) errors.push('Username does not exist.');
+    } catch (err) {
+        errors.push('There was a problem validating the login.');
+    }
+
+    //Send response indicating bad request if there are errors
+    if (errors.length > 0) {
+        res.status(400).json({ errors });
+        return;
+    }
+
+    //Authenticate user based on provided password
+    try {
+        const result = await db.query('SELECT password FROM appuser WHERE username=$1;', [username]);
+        const passwordHash = result.rows[0].password;
+        console.log(passwordHash);
+    } catch (err) {
+        errors.push('There was a problem validating user credentials.');
+        res.status(500).json({ errors });
+        return;
+    }
+
+    res.json({
+        message: 'successfully logged in'
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const app = express();
 
 const users = require('./routes/api/users');
 const ingredients = require('./routes/api/ingredients');
+const auth = require('./routes/api/auth');
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
+//Set up API routes
 app.use('/api/users', users);
 app.use('/api/ingredients', ingredients);
+app.use('/api/auth', auth);
 
+//Start up server
 app.listen(port, () => console.log(`Server started on port ${port}.`));
