02f1d163d0e4cfcb8bc6cb62f84904a3420ad4ed
Andrew Gerrard <argerrard@live.com>
2019-07-14 21:58:09 -0400

    updated signup route so that the form only shows if you are not logged in

--- README.md
+++ README.md
@@ -7,4 +7,3 @@ I will be building this app to help me practice building a React/Redux CRUD appl
 * Add support for optional nutrition fields when adding ingredients
 * Paginate fetched ingredient results
 * Change IngredientList so edit and delete buttons are only shown to users with the ability to do the actions
-* Add sign-up functionality
--- client/src/components/auth/SignUp.js
+++ client/src/components/auth/SignUp.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import SignUpForm from './SignUpForm';
 
 class SignUp extends React.Component {
 
+    renderSignUp() {
+        return <SignUpForm />;
+    }
+
     render() {
-        return (
-            <div>Signup</div>
-        );
+        if (this.props.isLoggedIn) {
+            return <div>You have already signed up. Please log out to make a new account.</div>;
+        } else {
+            return (
+                <div>
+                    {this.renderSignUp()}
+                </div>
+            );
+        }
     }
 
 }
 
-export default SignUp;
+const mapStateToProps = (state) => {
+    return {
+        isLoggedIn: state.auth.isLoggedIn
+    }
+};
+
+export default connect(mapStateToProps)(SignUp);
